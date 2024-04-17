69c6e10cd3b388582e262f28cbd7cb711b3f94f6
Hypes-Astro <muhamadalifanwar@gmail.com>
2024-04-17 11:59:58 +0700

    backup

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::get('/mahasiswa', function () {
+    return view('student.dashboardStudent');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
